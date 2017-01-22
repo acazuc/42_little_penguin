2779a3862e489bebe154671378f1fea8ec4dc312
Jukino <acazuc@student.42.fr>
2017-01-22 18:24:29 +0100

    Moved

--- assignment_01/main.c
+++ assignment_01/main.c
@@ -1,7 +1,11 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-int __init init_module(void)
+MODULE_LICENSE("GPL")
+MODULE_AUTHOR("Aurelien Cazuc <acazuc@student.42.fr>")
+MODULE_DESCRIPTION("Basic module")
+
+int init_module(void)
 {
 	pr_info("Hello world !\n");
 	return 0;
